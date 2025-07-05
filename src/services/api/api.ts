13290e25e09da70a801d97e4dd470d02ab57d531
lucasatdriano <lucasatdriano@gmail.com>
2025-07-05 18:08:05 -0300

    fix: url server

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,14 +1,17 @@
 import axios, { AxiosError } from 'axios';
 
-const API_BASE_URL =
-    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v1';
+const isServer = typeof window === 'undefined';
+
+const API_BASE_URL = isServer
+    ? process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v1/'
+    : 'https://fagon-server.onrender.com/api/v1/';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
-    timeout: 5000, // 5 segundos
+    timeout: 15000, // 15 segundos
 });
 
 export const extractAxiosError = (error: unknown) => {
